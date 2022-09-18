d0b977027d9ebca20fcdcf0febe9fd6bab08dbe2
Mejdi Ben Ammou <mejdi.ben.ammou@gmail.com>
2022-09-18 17:49:36 +0200

    deploy

--- client/src/Axios.js
+++ client/src/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.NODE_ENV === 'development' ? "http://localhost:5000" : "https://orenda.adaptable.app"
+const baseUrl = process.env.NODE_ENV === 'development' ? "http://localhost:5000" : "https://easy-chat.adaptable.app"
 
 export const Axios = axios.create({
     baseURL: baseUrl
